7265505f3bb697f4292ce496bb17934d9b4204d6
Xinshen Zheng <xinshenzheng00@gmail.com>
2025-05-31 20:54:43 +0200

    31052025 xz removed5

--- app/login/pdfViewer.tsx
+++ app/login/pdfViewer.tsx
@@ -6,9 +6,23 @@ import { useLocalSearchParams } from 'expo-router';
 export default function PDFViewer() {
   const { pdfUri } = useLocalSearchParams();
 
+  const injectedJS = `
+    const meta = document.createElement('meta');
+    meta.name = 'viewport';
+    meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes';
+    document.getElementsByTagName('head')[0].appendChild(meta);
+    true;
+  `;
+
   return (
     <View style={styles.container}>
-      <WebView source={{ uri: pdfUri }}  />
+      <WebView
+        source={{ uri: pdfUri }}
+        style={styles.webview}
+        injectedJavaScript={Platform.OS !== 'web' ? injectedJS : ''}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+      />
     </View>
   );
 }
